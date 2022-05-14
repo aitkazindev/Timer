371e49cef6ea6170384ea240e2def74b0a9f4929
aitkazindev <aitkazindev@gmail.com>
2022-05-14 13:23:18 +0900

    fixed bug with setting empty time

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -17,8 +17,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func setTime(_ sender: Any) {
+        let time = Int(textField.text!) ?? 0
         let detailVC = (storyboard?.instantiateViewController(withIdentifier: "timerVC"))! as! SecondViewController
-        detailVC.totalTime = Int(textField.text!)!
+        detailVC.totalTime = time
         self.navigationController?.pushViewController(detailVC, animated: true)
         
     }
